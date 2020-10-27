237d94495fb11a0629aa36c54dbfa6b3fd59e302
mashaaaa1 <poturaevamasha@icloud.com>
2020-10-27 11:55:03 +0300

    fix report and code

--- Poturaeva/course/curs.cpp
+++ Poturaeva/course/curs.cpp
@@ -6,7 +6,8 @@
 #include <stdlib.h>
 #include <time.h>
 using namespace std;
-class Graph {
+class Graph
+{
 private:
     int** matrix;
 public:
@@ -16,7 +17,7 @@ public:
     ~Graph() {
         for (int i = 0; i < N; ++i)
             delete[] matrix[i];
-        delete[] matrix; 
+        delete[] matrix;
     }
 
     void FloydWarshall() {
@@ -50,55 +51,70 @@ public:
             cout << "Wrong value for edges, enter number again:" << endl;
             cin >> edges;
         }
+
+        int tmp = 0;
+        cout << "If you want to enter the graph manually - press '1', if you want to generate a graph - press '2'. " << endl;
+        cin >> tmp;
         matrix = new int* [N];
 
-        for (int i = 0; i < N; i++) 
+        for (int i = 0; i < N; i++) // создание каждого одномерного массива в динамическом двумерном массиве, или иначе - создание столбцов размерность n
             matrix[i] = new int[N];
+        if (tmp == 2) {
+            const int R_MIN = -1;
+            const int R_MAX = 50;
+            int i, j;
+            int* randomArray = new int[edges];
+            srand(time(NULL));
 
-        const int R_MIN = -1;
-        const int R_MAX = 50;
-        int i, j;
-        int* randomArray = new int[edges];
-        srand(time(NULL));
-        
-        for (int i = 0; i < edges; i++) {
-            randomArray[i] = rand() % (R_MAX - R_MIN + 1) + R_MIN;
-            if (randomArray[i] == 0) {
-                i--;
+            for (int i = 0; i < edges; i++) {
+                randomArray[i] = rand() % (R_MAX - R_MIN + 1) + R_MIN;
+                if (randomArray[i] == 0) {
+                    i--;
+                }
+            }
+
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    matrix[i][j] = -1;
+                    if (i == j)
+                        matrix[i][j] = 0;
+                }
+            }
+
+            for (int i = 0; i < edges; i++) {
+                int k = rand() % (N);
+                int j = rand() % (N);
+                if (matrix[k][j] > 0 || k == j) {
+                    i--;
+                }
+                else {
+                    matrix[k][j] = randomArray[i];
+                }
+
             }
         }
-        
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                matrix[i][j] = -1;
-                if (i == j)
-                    matrix[i][j] = 0;
+        else if (tmp == 1) {
+            cout << "Fill in the adjacency matrix:" << endl;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++)
+                {
+                    cout << "matrix[" << i << "][" << j << "] = ";
+                    cin >> matrix[i][j];
+                }
             }
-        }
-
-        for (int i = 0; i < edges; i++) {
-            int k = rand() % (N);
-            int j = rand() % (N);
-            if (matrix[k][j] > 0 || k==j) {
-                i--;
-            }
-            else {
-                matrix[k][j] = randomArray[i];
-            }
-
         }
     }
 
     void Print(bool flag) {
         FILE* f;
-        if(flag)
-             f = fopen("before.txt", "w");
+        if (flag)
+            f = fopen("before.txt", "w");
         else
-             f = fopen("after.txt", "w");
+            f = fopen("after.txt", "w");
         fprintf(f, "[\\]");
         for (int i = 0; i < N; ++i)
-            fprintf( f,"[%d] ",i );
+            fprintf(f, "[%d] ", i);
         fprintf(f, "\n");
         for (int i = 0; i < N; i++) {
             fprintf(f, "[%d]", i);
@@ -114,14 +130,15 @@ public:
 
 int main()
 {
-    
+
     Graph graph;
     unsigned int tmp1 = clock();
     graph.Print(true);
     graph.FloydWarshall();
     graph.Print(false);
     unsigned int tmp2 = clock();
-    cout << "Runtime of program: "<<(tmp2 - tmp1)/1000.0 << endl;
+    cout << "Runtime of program: " << (tmp2 - tmp1) / 1000.0 << endl;
     cout << "The result of the program is in the file after.txt" << endl;
+    system("PAUSE");
     return 0;
 }
